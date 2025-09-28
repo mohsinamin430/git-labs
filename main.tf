7637943006e131fd6a4c4193cada7356b557218d
mohsinamin430 <mohsinamin430@gmail.com>
2025-09-28 08:37:31 +0100

    terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-provider "aws"      { region = "us-east-1" }
+provider "aws" { region = "us-east-1" }
 
 resource "aws_s3_bucket" "demo_ab12" {
-          bucket = "demo-bucket-ab12"
-  acl   = "private"
+  bucket = "demo-bucket-ab12"
+  acl    = "private"
 }
